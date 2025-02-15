26480750f108c43bfe7367c0cbf268a97e0241bd
Matheus Serra Nascimento <ggmat304@gmail.com>
2025-02-15 18:47:15 -0300

    Add files via upload
    Adição de arquivos.

---
+++ Pessoa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExercicioIMC;
+
+public class Pessoa
+{
+    public string nome;
+    public double peso, altura;
+
+    public double IMC()
+    {
+        double result = peso / Math.Pow(altura, 2);
+        return Math.Round(result, 2);
+    }
+
+    public string situacao(double imc)
+    {
+        imc = IMC();
+        if (imc < 18.5)
+        {
+            return "Abaixo do peso";
+        }
+
+        else if (imc < 25)
+        {
+            return "Peso normal";
+        }
+
+        else if (imc < 30)
+        {
+            return "Acima do peso";
+        }
+
+        else if (imc < 35)
+        {
+            return "Obesidade nivel I";
+        }
+
+        else if (imc < 40)
+        {
+            return "Obesidade nivel II";
+        }
+
+        else
+        {
+            return "Obesidade nivel III";
+        }
+    }
+
+    public void Msg()
+    {
+        double obterIMC = IMC();
+        string obterSituacao = situacao(obterIMC);
+
+        Console.WriteLine(nome + ", seu IMC é " + obterIMC + " seu estado é " + obterSituacao);
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using ExercicioIMC;
+
+namespace ExercicioIMC;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Pessoa pessoa = new Pessoa();
+        Console.WriteLine("Qual seu nome ?");
+        pessoa.nome = Console.ReadLine();
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Digite seu peso :");
+        pessoa.peso = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine("Digite sua altura :");
+        pessoa.altura = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("--------------------------------------");
+
+        Console.WriteLine(pessoa.IMC());
+        pessoa.Msg();
+    }
+}
